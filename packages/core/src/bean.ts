dca0f008bd853f19b0346f61613d49fffeec5581
mondodavid <david@madebymondo.com>
2023-04-28 14:54:48 -1000

    feat: parses config values

--- packages/core/src/bean.ts
+++ packages/core/src/bean.ts
@@ -1,19 +1,23 @@
 import chalk from "chalk";
 
-export default class Bean {
-  config: BeanConfig;
+export class Bean {
   /**
    *  List of paths to generate. Used for SSG
    * */
   paths?: string[];
+  pagesDirectory: BeanConfig["pagesDirectory"];
+  buildOutputPath: BeanConfig["buildOutputPath"];
+  passthroughDirectories: BeanConfig["passthroughDirectories"];
   renderMode: BeanConfig["renderMode"];
   templateEngine: BeanConfig["templateEngine"];
 
   constructor(config: BeanConfig) {
     /* Configuration */
-    this.config = config;
-    this.renderMode = this.config.renderMode;
-    this.templateEngine = this.config.templateEngine;
+    this.pagesDirectory = config.pagesDirectory || "pages";
+    this.buildOutputPath = config.buildOutputPath || "dist";
+    this.passthroughDirectories = config.passthroughDirectories || [];
+    this.renderMode = config.renderMode || "ssg";
+    this.templateEngine = config.templateEngine || "njk";
 
     this.paths = [];
   }
@@ -21,7 +25,16 @@ export default class Bean {
   /**
    *  Runs an Express server in dev
    * */
-  serve() {}
+  serve() {
+    console.log(chalk.green("running development server"));
+    console.log({
+      pages: this.pagesDirectory,
+      build: this.buildOutputPath,
+      passthrough: this.passthroughDirectories,
+      renderMode: this.renderMode,
+      templateEngine: this.templateEngine,
+    });
+  }
 
   /**
    *   Builds an output of files
