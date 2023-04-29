c04588e57c7b5dd5244d48cc7c589b9df0db3a5c
mondodavid <david@madebymondo.com>
2023-04-29 02:26:01 -1000

    refactor: update method of running config

--- packages/core/src/cmd.ts
+++ packages/core/src/cmd.ts
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 import "dotenv/config";
 import { Bean } from "./index.js";
-import { compileAndRunTS } from "./utils/compleAndRunTs.js";
+import { compileAndRunTS } from "./utils/compileAndRunTs.js";
 import { Command } from "commander";
 import path from "path";
 
 const program = new Command();
 
 const beanConfigPath = path.join(process.cwd(), "bean.config.ts");
-const beanConfig = await compileAndRunTS(beanConfigPath);
-const bean = new Bean(beanConfig[0]);
+const beanConfigFunctions = await compileAndRunTS(beanConfigPath);
+const beanConfig = beanConfigFunctions.find((func) => func.key === "default");
+
+const bean = new Bean(beanConfig.callback());
 
 program
   .command("dev")
   .description("Starts development server for the Bean site")
-  .action(() => {
-    bean.serve();
+  .action(async () => {
+    await bean.serve();
   });
 
 program.parse();
