ab99b3af3d9a543598ef9a563b6f43d3a9d749dc
mondodavid <david@madebymondo.com>
2023-05-04 23:40:06 -1000

    feat: add start command

--- packages/core/src/cmd.ts
+++ packages/core/src/cmd.ts
@@ -4,13 +4,15 @@ import { Bean } from "./index.js";
 import { compileAndRunTS } from "./utils/compileAndRunTs.js";
 import { Command } from "commander";
 import path from "path";
+import { exec } from "child_process";
+import chalk from "chalk";
 
 const program = new Command();
 
 const beanConfigPath = path.join(process.cwd(), "bean.config.ts");
 const beanConfigFunctions = await compileAndRunTS(beanConfigPath);
 const beanConfig = beanConfigFunctions.find((func) => func.key === "default");
-const { renderMode } = beanConfig.callback();
+const { renderMode, b } = beanConfig.callback();
 
 const bean = new Bean(beanConfig.callback());
 
@@ -26,6 +28,28 @@ program
   .description("Builds output for the Bean site")
   .action(async () => {
     await bean.build(renderMode || "ssg");
+  });
+
+program
+  .command("start")
+  .description("Starts server app")
+  .action(() => {
+    console.log(`Starting server...`);
+    const serverProcess = exec(
+      `node ${path.join(process.cwd(), "dist", "server.js")}`
+    );
+
+    serverProcess.stdout.on("data", (data) => {
+      console.log(chalk.blue(`[Server Process]: ${data.toString()}`));
+    });
+
+    serverProcess.stderr.on("data", (data) => {
+      console.error(chalk.red(`[Server Error]: ${data.toString()}`));
+    });
+
+    serverProcess.on("exit", (code) => {
+      console.log(chalk.blue(`[Server Exit]: ${code.toString()}`));
+    });
   });
 
 program.parse();
